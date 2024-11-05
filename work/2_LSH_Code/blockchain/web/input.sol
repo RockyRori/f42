aa908f97a2c563dbf904a35d3408d1d07c04390b
RockyRori <rockyrori@ado.com>
2024-11-05 22:20:08 +0800

    [feature]add solution doc

--- work/2_LSH_Code/blockchain/web/input.sol
+++ work/2_LSH_Code/blockchain/web/input.sol
@@ -1,29 +1,68 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.28;
 
-contract AccessControlVulnerability {
-    address public owner;
-    uint256 public funds;
+import "./IERC20.sol";
 
-    constructor() {
-        // 合约的部署者为合约的所有者
-        owner = msg.sender;
+contract F42Token is IERC20 {
+    // Token basic info.
+    string public name = "F42Token";
+    string public symbol = "F42";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    // Record balance of certain address.
+    mapping(address => uint256) public balanceOf;
+    // Record limit of certain address.
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // Set total coin via user input.
+    // Give them to creater address.
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply * 10**uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function deposit() public payable {
-        funds += msg.value;
+    // Implementation of interface.
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 
-    function withdraw(uint256 amount) public {
-        // 未进行访问控制，任何人都可以调用此函数
-        require(amount <= funds, "Insufficient funds");
-        (bool success, ) = msg.sender.call{value: amount}("");
-        require(success, "Transfer failed");
+    // Implementation of interface.
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0), "Invalid address");
 
-        funds -= amount;
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
     }
 
-    function getFunds() public view returns (uint256) {
-        return funds;
+    // Implementation of interface.
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
     }
-}
+}
